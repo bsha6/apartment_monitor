4a085b06b964da386232311a5319bb9ffca12777
Blake Sha <blake@Blakes-Air.fios-router.home>
2024-06-20 11:49:08 -0400

    Refactor to return beautiful soup object instead of response.text, wrap html using StringIO to avoid warning, test for second apartment website.

--- apt_webscraper_requests.py
+++ apt_webscraper_requests.py
@@ -1,34 +1,42 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+from io import StringIO
 
 
-def get_html_content(url: str) -> str:
-    """Given a url, make a get request and return the response text."""
+def get_soup(url: str) -> BeautifulSoup:
+    """Given a url, make a get request and return the BeautifulSoup object of the response content."""
     # Fetch the HTML content
     response = requests.get(url)
-    return response.text
+    soup = BeautifulSoup(response.text, 'html.parser')
+    return soup
 
 
-def parse_table_from_html(html_content: str, div_id: str) -> pd.DataFrame:
+def parse_table_from_html(soup: BeautifulSoup, div_id: str) -> pd.DataFrame:
     """
     Given the html of a webpage and a div id, extract a table as a pandas df.
     Note: this assumes a table is nested inside a div id.
     """
-    soup = BeautifulSoup(html_content, 'html.parser')
     div_apts = soup.find('div', id=div_id)
     if div_apts is None:
         raise ValueError(f"Div with id {div_id} not found")
     table = div_apts.find('table')
     if table is None:
         raise ValueError(f"No table found inside div with id {div_id}")
-    df = pd.read_html(str(table))[0]
+    df = pd.read_html(StringIO(str(table)))[0]
     return df
 
 lydian_url = "https://lydianlyric.com/lydian-floor-plans-2/?type=2BR"
-lydian_html = get_html_content(lydian_url)
+lydian_soup = get_soup(lydian_url)
 
 lydian_div_id = "floor-plans"
-lydian_df = parse_table_from_html(html_content=lydian_html, div_id=lydian_div_id)
+lydian_df = parse_table_from_html(soup=lydian_soup, div_id=lydian_div_id)
 
 print(lydian_df)
+
+lyric_url = "https://lydianlyric.com/lyric-floor-plans/?type=2BR"
+lyric_soup = get_soup(lyric_url)
+
+lyric_df = parse_table_from_html(soup=lyric_soup, div_id=lydian_div_id)
+
+print(lyric_df)
