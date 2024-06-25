db12bbb261dd2e5ebc51cc27d1af068f9546089c
Blake Sha <blake@Blakes-Air.fios-router.home>
2024-06-24 22:32:58 -0400

    WIP: add functions for closing conn and getting all cols in a table. Use try except structure

--- store_in_db.py
+++ store_in_db.py
@@ -26,11 +26,32 @@ class DBConfigManager:
     def __init__(self, db_connection):
         self.conn = db_connection
 
-    def get_all_from_table(self, table):
+    def close_connection(self):
+        if self.conn:
+            self.conn.close()
+            print("Database connection closed.")
+    
+    def get_all_cols_in_table(self, table: str):
+        """Given a table name, return a list of all the table's column names."""
+        try:
+            # Get table columns from the database
+            with self.conn.cursor() as cur:
+                cur.execute(f"""
+                    SELECT column_name 
+                    FROM information_schema.columns 
+                    WHERE table_name = %s
+                """, (table,))
+                db_columns = [row[0] for row in cur.fetchall()]
+                return db_columns
+        except psycopg2.Error as e:
+            print("Error: ", e)
+
+    def select_all_rows_from_table(self, table):
         with self.conn.cursor(cursor_factory=RealDictCursor) as cur:
             cur.execute(f"SELECT * FROM {table}")
             return cur.fetchall()
 
+    # TODO: modify this to work with a building_name arg
     def get_config_by_url(self, url):
         with self.conn.cursor(cursor_factory=RealDictCursor) as cur:
             cur.execute("SELECT * FROM apts WHERE url = %s", (url,))
@@ -54,35 +75,53 @@ class DBConfigManager:
 
 
 if __name__ == "__main__":
-    conn = psycopg2.connect(**db_params)
-    config_manager = DBConfigManager(conn)
-    apts = config_manager.get_all_from_table("apts")
-    apts_df = pd.DataFrame(apts)
+    try:
+        conn = psycopg2.connect(**db_params)
+        config_manager = DBConfigManager(conn)
+        apts = config_manager.select_all_rows_from_table("apts")
+        apts_df = pd.DataFrame(apts)
 
-    # Test comparing new data with historical.
-    lyric_apt_df = apts_df[apts_df['building_name'] == 'Lyric']
+        # for row in all_configs:
+        #     if row['div_id']:
+        #         print(scrape_parse_and_read_html(url=row['url'], div_id=row['div_id']))
+        #     else:
+        #         print(interact_scrape_and_get_df(url=row['url']))
 
-    lyric_apt_id = lyric_apt_df['id'].iloc[0]
-    lyric_url = lyric_apt_df['url'].iloc[0]
-    lyric_div_id = lyric_apt_df['div_id'].iloc[0]
+        # TODO: store scraped data in db !
+        fp_cols = config_manager.get_all_cols_in_table("floor_plans")
+        print(fp_cols)
 
-    latest_lyric_df = scrape_parse_and_read_html(url=lyric_url, div_id=lyric_div_id)
-    print(latest_lyric_df)
+        # TODO: add update_floor_plans_table()
+        # TODO: will need to add columns apt_id and availability status (or make this True by default?, then change this to false when fp_id isn't in new batch of data?)
 
-    # Lookup old data from floor plans table
-    conn = psycopg2.connect(**db_params)
-    config_manager = DBConfigManager(conn)
-    floor_plans = config_manager.get_all_from_table("floor_plans")
-    # TODO: do filtering in SQL
-    floor_plans_df = pd.DataFrame(floor_plans)
-    lyric_fps = floor_plans_df[floor_plans_df["apt_id"] == lyric_apt_id]
-    print(lyric_fps)
+        # WIP: Test comparing new data with historical.
+        # lyric_apt_df = apts_df[apts_df['building_name'] == 'Lyric']
+
+        # lyric_apt_id = lyric_apt_df['id'].iloc[0]
+        # lyric_url = lyric_apt_df['url'].iloc[0]
+        # lyric_div_id = lyric_apt_df['div_id'].iloc[0]
+
+        # latest_lyric_df = scrape_parse_and_read_html(url=lyric_url, div_id=lyric_div_id)
+        # print(latest_lyric_df)
+
+        # # Lookup old data from floor plans table
+        # conn = psycopg2.connect(**db_params)
+        # config_manager = DBConfigManager(conn)
+        # floor_plans = config_manager.select_all_rows_from_table("floor_plans")
+        # # TODO: filter data in SQL
+        # floor_plans_df = pd.DataFrame(floor_plans)
+        # lyric_fps = floor_plans_df[floor_plans_df["apt_id"] == lyric_apt_id]
+        # print(lyric_fps)
+
+    except psycopg2.Error as e:
+        print(f"Error connecting to the database: {e}")
+
+    finally:
+        # Close the connection when done
+        if 'config_manager' in locals():
+            config_manager.close_connection()
 
 
 
 
-    # for row in all_configs:
-    #     if row['div_id']:
-    #         print(scrape_parse_and_read_html(url=row['url'], div_id=row['div_id']))
-    #     else:
-    #         print(interact_scrape_and_get_df(url=row['url']))
+
